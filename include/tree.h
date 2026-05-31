d42f82483e090cbd0be6a2a5adf83bd433052505
Mirghit <yuandru25@gmail.com>
2026-05-31 22:55:12 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,28 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+
+class PMTree {
+ public:
+    struct Node {
+        char val;
+        std::vector<Node*> childs;
+        explicit Node(char v) : val(v) {}
+    };
+    Node *root;
+    int size;
+    explicit PMTree(const std::vector<char>& chs);
+    ~PMTree();
+
+ private:
+  Node* buildSubtree(const std::vector<char>& other);
+  void deleteNode(Node *node);
+};
+
+std::vector<std::vector<char>> getAllPerms(const PMTree &tree);
+std::vector<char> getPerm1(const PMTree &tree, int loc);
+std::vector<char> getPerm2(const PMTree &tree, int loc);
+unsigned long long factorial(int n);
+
 #endif  // INCLUDE_TREE_H_
